e3b0dbdf4a9d0d88dd52ebbf20b61ff66b7e5a3d
Fedor <bysov1987@mail.ru>
2022-11-17 00:19:58 +0300

    location and search Work

--- app/src/main/java/com/example/weatherappavito/presenation/SearchCityFragment.kt
+++ app/src/main/java/com/example/weatherappavito/presenation/SearchCityFragment.kt
@@ -1,25 +1,27 @@
 package com.example.weatherappavito.presenation
 
 import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
 import com.example.weatherappavito.R
 import com.example.weatherappavito.databinding.FragmentSearchCityBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.NonDisposableHandle.parent
 
 
 class SearchCityFragment : Fragment() {
 
+    private lateinit var viewModel: WeatherViewModel
     private var _binding: FragmentSearchCityBinding? = null
     private val binding
         get() = _binding!!
@@ -36,82 +38,47 @@ class SearchCityFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
 
 
+//        binding.etSearch.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                viewModel.resetInputName()
+//            }
+//
+//            override fun afterTextChanged(p0: Editable?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
 
-//        {
-//
-//
-//            private LocationManager mLocationManager;
-//            private LocationListener mLocationListener;
-//            private Location mLocation;
-//            private TextView mLatitudeTextView, mLongitudeTextView;
-//
-//            private static final long MINIMUM_DISTANCE_FOR_UPDATES = 10; // в метрах
-//            private static final long MINIMUM_TIME_BETWEEN_UPDATES = 2000; // в мс
-//
-//
-//            public void onCreate(Bundle savedInstanceState) {
-//                super.onCreate(savedInstanceState);
-//
-//                setContentView(R.layout.activity_main);
-//
-//                mLatitudeTextView = (TextView) findViewById (R.id.textViewLatitude);
-//                mLongitudeTextView = (TextView) findViewById (R.id.textViewLongitude);
-//
-//                mLocationManager = (LocationManager) getSystemService (Context.LOCATION_SERVICE);
-//
-//                Criteria criteria = new Criteria();
-//                criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-//                criteria.setPowerRequirement(Criteria.POWER_LOW);
-//                criteria.setAltitudeRequired(false);
-//                criteria.setBearingRequired(false);
-//                criteria.setCostAllowed(true);
-//                String provider = mLocationManager . getBestProvider (criteria, true);
-//
-//                mLocation = mLocationManager.getLastKnownLocation(provider);
-//                mLocationListener = new MyLocationListener ();
-//
-//                showCurrentLocation(mLocation);
-//
-//                // Регистрируемся для обновлений
-//                mLocationManager.requestLocationUpdates(
-//                    provider,
-//                    MINIMUM_TIME_BETWEEN_UPDATES, MINIMUM_DISTANCE_FOR_UPDATES,
-//                    mLocationListener
-//                );
-//            }
-//
-//            @Override
-//            public void onPause() {
-//                super.onPause();
-//                mLocationManager.removeUpdates(mLocationListener);
-//            }
-//
-//            public void onClick(View v) {
-//                showCurrentLocation(mLocation);
-//            }
-//
-//            protected void showCurrentLocation(Location location) {
-//                if (location == null) {
-//                    mLatitudeTextView.setText("Не работает");
-//                    mLongitudeTextView.setText("Не работает");
-//                }
-//                if (location != null) {
-//                    mLatitudeTextView.setText(String.valueOf(location.getLatitude()));
-//                    mLongitudeTextView.setText(String.valueOf(location.getLongitude()));
-//                }
-//
-//            }
-//
-//
-//
+//        viewModel.errorInputName.observe(viewLifecycleOwner){
+//            TODO("Сделаь обработку ошибок!!!")
+//        }
+
+        binding.clBackground.setOnClickListener {
+            binding.clBackground.hideKeyboard()
+        }
+
+        binding.btnSearch.setOnClickListener {
+
+            viewModel.loadData(binding.etSearch.text.toString())
+            binding.btnSearch.hideKeyboard()
+            Thread.sleep(200)
+            requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
+        }
+
 
     }
 
 
-
-    companion object{
-
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
+
 }
--- app/src/main/java/com/example/weatherappavito/presenation/WeatherDetailedFragment.kt
+++ app/src/main/java/com/example/weatherappavito/presenation/WeatherDetailedFragment.kt
@@ -1,27 +1,20 @@
 package com.example.weatherappavito.presenation
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.Location
 import android.location.LocationRequest
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.example.weatherappavito.R
 import com.example.weatherappavito.databinding.FragmentWeatherDetailedBinding
 import com.example.weatherappavito.presenation.adaapters.AdapterWeatherHour
 import com.example.weatherappavito.presenation.adaapters.AdapterWeatherWeek
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.coroutines.launch
-import java.util.*
 
 
 class WeatherDetailedFragment : Fragment() {
@@ -44,7 +37,10 @@ class WeatherDetailedFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentWeatherDetailedBinding.inflate(layoutInflater)
         return binding.root
+
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -53,14 +49,10 @@ class WeatherDetailedFragment : Fragment() {
         val adapterHour = AdapterWeatherHour()
         val adapterWeek = AdapterWeatherWeek()
 
-//        fusedLocationProviderClient =
-//            LocationServices.getFusedLocationProviderClient(requireContext())
-//
-//        requestPermission(requireContext())
-//        getLastLocation(requireContext())
         lifecycleScope.launch {
-            location(requireContext())
+            viewModel.location(requireContext(),requireActivity(), REQUEST_CODE)
         }
+
 
         binding.rvWeatherHour.adapter = adapterHour
         binding.rvWeatherWeek.adapter = adapterWeek
@@ -87,150 +79,27 @@ class WeatherDetailedFragment : Fragment() {
         }
 
 
+        binding.fbSearchCity.setOnClickListener {
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .addToBackStack(NAME)
+                .add(R.id.weather_detailed_container, SearchCityFragment())
+                .commit()
+        }
 
         binding.fbLocationDetermination.setOnClickListener {
             lifecycleScope.launch {
-                location(requireContext())
+                viewModel.location(requireContext(),requireActivity(), REQUEST_CODE)
             }
         }
 
     }
 
-    suspend fun location(context: Context) {
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireContext())
-        requestPermission(context)
-        getLastLocation(context)
-    }
-
-    suspend fun getLastLocation(context: Context) {
-        if (сheckPermission(context)) {
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(
-                    context,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
-                    val location: Location? = task.result
-                    if (location == null) {
-                        lifecycleScope.launch {
-                            NewLocationData(context)
-                        }
-                    } else {
-                        Log.d(
-                            "Debug",
-                            "${
-                                lifecycleScope.launch {
-                                    getCityName(
-                                        location.latitude,
-                                        location.longitude,
-                                        context
-                                    )
-                                }
-                            }"
-                        )
-
-                    }
-                }
-            } else {
-                Toast.makeText(context, "Please Turn on Your device Location", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        } else {
-            requestPermission(context)
-        }
-    }
-
-
-    //1
-    suspend fun сheckPermission(context: Context): Boolean {
-        if (
-            ActivityCompat.checkSelfPermission(
-                context,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(
-                context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    //2
-
-    suspend fun NewLocationData(context: Context) {
-        var locationRequest = LocationRequest()
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireActivity())
-        if (
-            ActivityCompat.checkSelfPermission(
-                context,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(
-                context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            fusedLocationProviderClient!!.requestLocationUpdates(
-                locationRequest, locationCallback, Looper.myLooper()
-            )
-        }
-    }
-
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            var lastLocation: Location = locationResult.lastLocation!!
-            Log.d(
-                "Debug",
-                "${
-                    lifecycleScope.launch {
-                        getCityName(
-                            lastLocation.latitude,
-                            lastLocation.longitude,
-                            requireContext()
-                        )
-                    }
-                }"
-            )
-        }
-    }
-
-
-    //3
-    suspend fun getCityName(lat: Double, long: Double, context: Context): String {
-        var cityName: String = ""
-        val geoCoder = Geocoder(context, Locale.getDefault())
-        val Adress = geoCoder.getFromLocation(lat, long, 3)
-
-        cityName = Adress?.get(0)?.locality ?: "Moscow"
-        Log.d("Debug:", cityName)
-        return cityName
-    }
-
-
-    //4
-
-    suspend fun requestPermission(context: Context) {
-        ActivityCompat.requestPermissions(
-            requireActivity(),
-            arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ),
-            REQUEST_CODE
-        )
-    }
 
 
     companion object {
+
+        const val NAME = "info"
 
         private const val REQUEST_CODE = 101
 
